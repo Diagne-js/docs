b9b0939e2ec13e3d0f0f9ddc9f6dae5ea5c83c07
Diagne.js <cheikhounadiagne358@gmail.com>
2024-12-28 19:04:25 +0000

    STYLECHANGING

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -28,7 +28,7 @@ export const homepage = () => {
     document.keywords = "diagne, diagne.js, diagneJs, javascript, js framework, front-end"
     return`
       ${welcome()}
-      <div>
+      <div >
          <article d-for="argument in arguments" style="margin:60px 0;padding:0 50px;">
              <h2>{ argument.title }</h2>
              <p>{ argument.body }</p>
