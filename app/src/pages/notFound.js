a1173a19544062a3da6fb576684bc60aa648f2db
Diagne.js <cheikhounadiagne358@gmail.com>
2025-03-23 17:50:42 +0000

    Create notFound.js

---
+++ app/src/pages/notFound.js
@@ -0,0 +1,7 @@
+export const notFound = () => {
+  return `
+    <h1>404</h1>
+    <p style="text-align: center; font-size: 1.5vh">The Route ${window.location.pathname} Is Not Found!</p>
+   
+  `
+}
