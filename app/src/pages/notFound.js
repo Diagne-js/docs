93cf2b4f74ae4a3e4cae637ec6fff22caab421b9
Diagne.js <cheikhounadiagne358@gmail.com>
2025-03-23 18:12:55 +0000

    Update notFound.js

--- app/src/pages/notFound.js
+++ app/src/pages/notFound.js
@@ -1,5 +1,5 @@
 export const notFound = () => {
-  const linkStyle = "display: block;width: 350px; height: 50px; border: solid 3px white; border-radius: 10px; margin: 30px 10px; display: flex; justify-content: center; align-items: center; font-size: 1.1vh;"
+  const linkStyle = "display: block;width: 350px; height: 50px; border: solid 1px white; border-radius: 10px; margin: 30px 10px; display: flex; justify-content: center; align-items: center; font-size: 1.1vh;"
   
   return `
     <h1>404</h1>
@@ -7,7 +7,7 @@ export const notFound = () => {
     <br><br>
     <h2 style="color: white; text-align: left">Maybe You Wanna Go to...</h2>
      <br>
-    <a style="${linkStyle}" to="/"><h1>Home</h1></a>
-    <a style="${linkStyle}" to="/guide"><h1>Guide</h1></a>
+    <a style="${linkStyle}" to="/"><h2>Home</h2></a>
+    <a style="${linkStyle}" to="/guide"><h2>Guide</h2></a>
   `
 }
