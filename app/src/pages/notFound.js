a080a9e0962df3f6a008c3b7178b09b07937e667
Diagne.js <cheikhounadiagne358@gmail.com>
2025-03-23 18:10:06 +0000

    Update notFound.js

--- app/src/pages/notFound.js
+++ app/src/pages/notFound.js
@@ -4,8 +4,9 @@ export const notFound = () => {
   return `
     <h1>404</h1>
     <p style="text-align: center; font-size: 1.5vh">The Route ${window.location.pathname} Is Not Found!</p>
+    <br><br>
     <h2 style="color: white; text-align: left">Maybe You Wanna Go to...</h2>
-     <br><br>
+     <br>
     <a style="${linkStyle}" to="/"><h1>Home</h1></a>
     <a style="${linkStyle}" to="/guide"><h1>Guide</h1></a>
   `
